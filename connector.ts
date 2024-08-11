955ac5bd6df5742f25bcd9c3f34a75c8b843d3fd
Sean Crowe <retrounlocked@gmail.com>
2024-08-11 13:43:54 -0500

    Build a Simple Media Connector

---
+++ connector.ts
@@ -0,0 +1,88 @@
+import { Connector, Media } from "@chili-publish/studio-connectors";
+
+export default class MyConnector implements Media.MediaConnector {
+
+  private runtime: Connector.ConnectorRuntimeContext;
+
+  constructor(runtime: Connector.ConnectorRuntimeContext) {
+    this.runtime = runtime;
+  }
+
+  async query(
+    options: Connector.QueryOptions,
+    context: Connector.Dictionary
+  ): Promise<Media.MediaPage> {
+    const resp = await this.runtime.fetch("https://picsum.photos/v2/list?page=1", {
+      method: "GET"
+    });
+
+    if (resp.ok) {
+      const data = JSON.parse(resp.text);
+
+      // Transform the data to match the Media type
+      const dataFormatted = data.map(d => ({
+        id: d.id,
+        name: d.id,
+        relativePath: "/",
+        type: 0,
+        metaData: {}
+      })) as Array<any>;
+
+      return {
+        pageSize: options.pageSize, // Note: pageSize is not currently used by the UI
+        data: dataFormatted,
+        links: {
+          nextPage: "" // Pagination is ignored in this example
+        }
+      }
+    }
+
+    // Handle error case
+    throw new Error("Failed to fetch images from picsum.photos");
+  }
+
+  async detail(
+    id: string,
+    context: Connector.Dictionary
+  ): Promise<Media.MediaDetail> {
+    return {
+      name: id,
+      id: id,
+      metaData: {},
+      relativePath: "/",
+      type: 0
+    }
+  }
+
+  async download(
+    id: string,
+    previewType: Media.DownloadType,
+    intent: Media.DownloadIntent,
+    context: Connector.Dictionary
+  ): Promise<Connector.ArrayBufferPointer> {
+
+    // Check to see if we are a thumbnail in the UI or being used in another situation.
+    switch (previewType) {
+      case "thumbnail": {
+        const picture = await this.runtime.fetch(`https://picsum.photos/id/${id}/200`, { method: "GET" });
+        return picture.arrayBuffer;
+      }
+      default: {
+        const picture = await this.runtime.fetch(`https://picsum.photos/id/${id}/1000`, { method: "GET" });
+        return picture.arrayBuffer;
+      }
+    }
+  }
+
+  getConfigurationOptions(): Connector.ConnectorConfigValue[] | null {
+    return [];
+  }
+  getCapabilities(): Media.MediaConnectorCapabilities {
+    return {
+      query: true,
+      detail: true,
+      filtering: true,
+      metadata: false,
+    };
+  }
+}
